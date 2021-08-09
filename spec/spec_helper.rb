479b6da1818b5ec49e369ca9e09e61bd4f82351f
personal <vincezipparro@gmail.com>
2021-08-09 14:40:47 -0500

    fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,4 @@
 require File.expand_path('../config/environment', __dir__)
-# require File.expand_path('../config/dt_env.rb', File.dirname(__FILE__))
 require 'rspec/rails'
 require 'capybara'
 require 'capybara/rspec'
